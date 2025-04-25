23aead02657d1d9192d0dcf2fb4c56248d625235
Hideyuki-T <hide27yuki@gmail.com>
2025-04-26 02:57:11 +0900

    [add]Array/Contains.sh | [upgrade]README.md

---
+++ src/app/lib/Array/Contains.sh
@@ -0,0 +1,8 @@
+Array::Contains() {
+  local element
+  for element in "${@:2}"　# 第2引数以降（検索対象の配列）をループ
+  do
+    [[ "$element" = "$1" ]] && return 0　# 見つかったら成功(0)を返す
+  done
+  return 1　# 見つからなければ失敗(1)を返す
+}
