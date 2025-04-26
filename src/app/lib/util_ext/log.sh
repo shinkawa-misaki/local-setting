5f15412eeaac4ac29380e529918ada67af583089
Hideyuki-T <hide27yuki@gmail.com>
2025-04-26 11:55:50 +0900

    [add]lib/util_ext/ | [upgrade]README.md

---
+++ src/app/lib/util_ext/log.sh
@@ -0,0 +1,169 @@
+#!/usr/bin/env bash
+set -e
+
+namespace util-ext/log
+
+## IMPORT ##
+# ------------------------ #
+import util/log
+
+## MAIN ##
+# ------------------------ #
+# ログメッセージを実際に出力するための関数
+# 色付きでログメッセージを標準出力に出す関数
+Logger::OutputMsg() {
+  local script="$1"
+  local lineNo=$2
+  local color=$3
+  local type=$4
+  shift; shift; shift; shift
+
+  echo "$color[$(date +"%Y/%m/%d %H:%M:%S")] $color[$type] $(UI.Color.Blue)[${script}:${lineNo}]$(UI.Color.Default) $* "
+}
+
+# 確認メッセージを標準エラー出力
+Logger::ConfirmMsg() {
+  local confirmMessage="$1"
+  shift;
+
+  Console::WriteStdErr "$(UI.Color.LightGreen)$confirmMessage $(UI.Color.Default)$* "
+}
+
+# ユーザー入力を促すメッセージを標準エラー出力
+Logger::InputMsg() {
+  Console::WriteStdErr "$(UI.Color.Yellow)[Please Enter] $(UI.Color.Default)$* "
+}
+
+# INFO レベルのローカルログを出力する関数
+Logger::LocalINFO() {
+  local output_type="$1"
+  local callingScript="$2"
+  local callingFunction="$3"
+  local infoMessage="$4"
+  local command="Console::WriteStdErrAnnotated"
+
+  # output_type が console 以外の場合は別の出力手段を指定する
+  if [[ "$output_type" != "console" ]]; then
+    command="Logger::OutputMsg"
+  fi
+
+  $command "$callingScript $callingFunction" "*" "$(UI.Color.Blue)" INFO "$infoMessage"
+}
+
+# WARN レベルのローカルログを出力する関数
+Logger::LocalWARNING() {
+  local output_type="$1"
+  local callingScript="$2"
+  local callingFunction="$3"
+  local warnMessage="$4"
+  local command="Console::WriteStdErrAnnotated"
+
+  # output_type が console 以外の場合は別の出力手段を指定する
+  if [[ "$output_type" != "console" ]]; then
+    command="Logger::OutputMsg"
+  fi
+
+  $command "$callingScript $callingFunction" "*" "$(UI.Color.LightCyan)" WARN "$warnMessage"
+}
+
+# ERROR レベルのローカルログを出力する関数
+Logger::LocalERROR() {
+  local output_type="$1"
+  local callingScript="$2"
+  local callingFunction="$3"
+  local callingLineNo="$4"
+  local errorMessage="$5"
+
+  local command="Console::WriteStdErrAnnotated"
+
+  # output_type が console 以外の場合は別の出力手段を指定する
+  if [[ "$output_type" != "console" ]]; then
+    command="Logger::OutputMsg"
+  fi
+
+  $command "$callingScript $callingFunction" "$callingLineNo" "$(UI.Color.Red)" ERROR "$errorMessage"
+}
+
+# INFO ログを出力するためのラッパ関数
+Logger::OutputInfoLog() {
+  local infoMessage="$1"
+  local output_type=${2:-console}
+  local bash_path=${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}
+  local bash_dir="${bash_path%/*}"
+
+  Logger::LocalINFO "$output_type" "${bash_dir##*/}/${bash_path##*/}" "${FUNCNAME[1]}" "$infoMessage"
+}
+
+# WARN ログを出力するためのラッパ関数
+Logger::OutputWarnLog() {
+  local warnMessage="$1"
+  local output_type=${2:-console}
+  local bash_path=${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}
+  local bash_dir="${bash_path%/*}"
+
+  Logger::LocalWARNING "$output_type" "${bash_dir##*/}/${bash_path##*/}" "${FUNCNAME[1]}" "$warnMessage"
+}
+
+# ERROR ログを出力し、必要に応じてスクリプトを終了する関数
+Logger::OutputErrorLog() {
+  local errorMessage="$1"
+  local output_type=${2:-console}
+  local bash_path=${3:-${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}}
+  local bash_dir="${4:-${bash_path%/*}}"
+  local function_name="${5:-${FUNCNAME[1]}}"
+
+  Logger::LocalERROR "$output_type" "${bash_dir##*/}/${bash_path##*/}" "$function_name" "${BASH_LINENO[2]}" "$errorMessage"
+
+  # output_type が console の場合のみスクリプトを終了
+  if [[ "$output_type" == "console" ]]; then
+    exit 1
+  fi
+}
+
+# 処理開始時のログを出力する関数
+Logger::OutputStartLog() {
+  local logMessage="$1"
+  local output_type=${2:-console}
+  local bash_path=${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}
+  local bash_dir="${bash_path%/*}"
+
+  # logMessage が空の場合は呼び出し元関数名を使用
+  if [[ ${#logMessage} -eq 0 ]]; then
+      logMessage=${FUNCNAME[1]}
+  fi
+
+  Logger::LocalINFO "$output_type" "${bash_dir##*/}/${bash_path##*/}" "${FUNCNAME[1]}" "START $logMessage"
+}
+
+# 処理結果をログに出力する関数
+Logger::OutputResultLog() {
+  local result="$1"
+  local output_type="$2"
+
+  # output_type が console/file 以外の場合は console に変更
+  if [[ "$output_type" != "console" && "$output_type" != "file" ]];then
+    shift;
+    output_type="console"
+  else
+    shift;shift;
+  fi
+
+  local logMessage="${*:-${FUNCNAME[1]}}"
+  local bash_path=${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}
+  local bash_dir="${bash_path%/*}"
+
+  # result が 0 の場合は成功ログ、それ以外はエラーログ
+  if [[ "$result" == "0" ]]; then
+    Logger::LocalINFO "$output_type" "${bash_dir##*/}/${bash_path##*/}" "${FUNCNAME[1]}" "END $logMessage Success"
+  else
+    Logger::OutputErrorLog "$logMessage is Failed" "$output_type" "$output_type" "$bash_path" "$bash_dir" "${FUNCNAME[1]}"
+  fi
+}
+
+alias confirmMsg='Logger::ConfirmMsg'
+alias inputMsg='Logger::InputMsg'
+alias outputInfoLog='Logger::OutputInfoLog'
+alias outputWarnLog='Logger::OutputWarnLog'
+alias outputErrorLog='Logger::OutputErrorLog'
+alias outputStartLog='Logger::OutputStartLog'
+alias outputResultLog='Logger::OutputResultLog'
---
+++ src/app/lib/util_ext/zsh.sh
@@ -0,0 +1,196 @@
+#!/usr/bin/env bash
+set -e
+
+# 「zshrc」や「zshrc_original」ファイルを操作するための専用ユーティリティ群
+
+namespace util-ext/zsh
+
+## IMPORT ##
+# ------------------------ #
+import util-ext/log
+import util-ext/grep
+import util-ext/gsed
+
+## MAIN ##
+# ------------------------ #
+# zshrc ファイルの初期設定を行う関数
+Zshrc::Init() {
+  # zshrc_path が存在しない場合にファイルを作成する
+  if [[ ! -f $zshrc_path ]]; then
+    outputInfoLog "touch $zshrc_path"
+    touch "$zshrc_path"
+  fi
+}
+
+# zshrc ファイルにワードを追加する関数
+Zshrc::AddWord() {
+  local word="$1"
+
+  # word の文字数が 0 の場合はエラーを出力する
+  if [[ "${#word}" -eq 0 ]]; then
+    outputErrorLog "word must be length over 1"
+  fi
+
+  # zshrc_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_path ]]; then
+    outputErrorLog "not found zshrc file:$zshrc_path"
+  fi
+
+  gsAddUniqueRow "$word" "$zshrc_path" "^" "zshrc add $word"
+}
+
+# zshrc ファイルからワードを削除する関数
+Zshrc::DeleteWord() {
+  local word="$1"
+
+  # word の文字数が 0 の場合はエラーを出力する
+  if [[ "${#word}" -eq 0 ]]; then
+    outputErrorLog "word must be length over 1"
+  fi
+
+  # zshrc_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_path ]]; then
+    outputErrorLog "not found zshrc file:$zshrc_path"
+  fi
+
+  outputStartLog "zshrc delete ${word//\\n/}"
+  gsDeleteLikeWord "$word" "$zshrc_path"
+  outputResultLog $? "zshrc delete ${word//\\n/}"
+}
+
+# 別ファイルを source 行として zshrc ファイルに追加する関数
+Zshrc::AddSourceFile() {
+  local source_path="$1"
+  local log_message="zshrc add ${source_path##*/}"
+
+  # source_path が存在しない場合はエラーを出力する
+  if [[ ! -f "$source_path" ]]; then
+    outputErrorLog "not found zshrc source file:$source_path"
+  fi
+
+  # zshrc_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_path ]]; then
+    outputErrorLog "not found zshrc file:$zshrc_path"
+  fi
+
+  gsAddUniqueRow "# import ${source_path##*/}" "$zshrc_path" "" "$log_message"
+  gsAddUniqueRow "source $source_path\n" "$zshrc_path" "" "$log_message"
+}
+
+# zshrc_original ファイルの初期設定を行う関数
+ZshrcOriginal::Init() {
+  # zshrc_original_path が存在しない場合にファイルを作成し、zshrc で読み込む
+  if [[ ! -f "$zshrc_original_path" ]]; then
+    outputInfoLog "touch $zshrc_original_path"
+    touch "$zshrc_original_path"
+  fi
+
+  Zshrc::AddSourceFile "$zshrc_original_path"
+}
+
+# zshrc_original ファイル内の文字列を置き換える関数
+ZshrcOriginal::ReplaceWord() {
+  local word="$1"
+  local replace_word="$2"
+
+  # word, replace_word が空の場合はエラーを出力する
+  if [[ "${#word}" -eq 0 ]] || [[ "${#replace_word}" -eq 0 ]]; then
+    outputErrorLog "word or replace_word must be length over 1"
+  fi
+
+  # zshrc_original_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_original_path ]]; then
+    outputErrorLog "not found zshrc_original file:$zshrc_original_path"
+  fi
+
+  outputStartLog "zshrc_original_path replace $word"
+  gsReplace "$word" "$replace_word" "$zshrc_original_path"
+  outputResultLog $? "zshrc_original_path replace $word"
+}
+
+# zshrc_original ファイル内の文字列を正規表現で置き換える関数
+ZshrcOriginal::ReplaceWordOfRegExp() {
+  local word="$1"
+  local replace_word="$2"
+
+  # word, replace_word が空の場合はエラーを出力する
+  if [[ "${#word}" -eq 0 ]] || [[ "${#replace_word}" -eq 0 ]]; then
+    outputErrorLog "word or replace_word must be length over 1"
+  fi
+
+  # zshrc_original_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_original_path ]]; then
+    outputErrorLog "not found zshrc_original file:$zshrc_original_path"
+  fi
+
+  outputStartLog "zshrc_original_path replace $word of RegExp"
+  gsReplaceOFRegExpFirst "$word" "$replace_word" "$zshrc_original_path"
+  outputResultLog $? "zshrc_original_path replace $word of RegExp"
+}
+
+# zshrc_original ファイルにワードを追加する関数
+ZshrcOriginal::AddWord() {
+  local word="$1"
+  local log_message="zshrcOriginal add $word"
+
+  # word の文字数が 0 の場合はエラーを出力する
+  if [[ "${#word}" -eq 0 ]]; then
+    outputErrorLog "word must be length over 1"
+  fi
+
+  # zshrc_original_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_original_path ]]; then
+    outputErrorLog "not found zshrc_original file:$zshrc_original_path"
+  fi
+
+  gsAddUniqueRow "$word" "$zshrc_original_path" "^" "$log_message"
+}
+
+# zshrc_original ファイルからワードを削除する関数
+ZshrcOriginal::DeleteWord() {
+  local word="$1"
+
+  # word の文字数が 0 の場合はエラーを出力する
+  if [[ "${#word}" -eq 0 ]]; then
+    outputErrorLog "word must be length over 1"
+  fi
+
+  # zshrc_original_path が存在しない場合はエラーを出力する
+  if [[ ! -f $zshrc_original_path ]]; then
+    outputErrorLog "not found zshrc_original file:$zshrc_original_path"
+  fi
+
+  outputStartLog "zshrcOriginal delete ${word//\\n/}"
+  gsDeleteLikeWord "$word" "$zshrc_original_path"
+  outputResultLog $? "zshrcOriginal delete ${word//\\n/}"
+}
+
+# 別ファイルを source 行として zshrc_original ファイルに追加する関数
+ZshrcOriginal::AddSourceFile() {
+  local source_path="$1"
+  local log_message="zshrcOriginal add ${source_path##*/}"
+
+  # source_path が存在しない場合はエラーを出力する
+  if [[ ! -f "$source_path" ]]; then
+    outputErrorLog "not found zshrc_original source file:$source_path"
+  fi
+
+  # zshrc_original_path が存在しない場合はエラーを出力する
+  if [[ ! -f "$zshrc_original_path" ]]; then
+    outputErrorLog "not found zshrc_original file:$zshrc_original_path"
+  fi
+
+  gsAddUniqueRow "# import ${source_path##*/}" "$zshrc_original_path" "" "$log_message"
+  gsAddUniqueRow "source $source_path\n" "$zshrc_original_path" "" "$log_message"
+}
+
+alias zshrcInit='Zshrc::Init'
+alias zshrcAddWord='Zshrc::AddWord'
+alias zshrcDeleteWord='Zshrc::DeleteWord'
+alias zshrcAddSourceFile='Zshrc::AddSourceFile'
+alias zshrcOriginalInit='ZshrcOriginal::Init'
+alias zshrcOriginalReplaceWord='ZshrcOriginal::ReplaceWord'
+alias zshrcOriginalReplaceWordOfRegExp='ZshrcOriginal::ReplaceWordOfRegExp'
+alias zshrcOriginalAddWord='ZshrcOriginal::AddWord'
+alias zshrcOriginalDeleteWord='ZshrcOriginal::DeleteWord'
+alias zshrcOriginalAddSourceFile='ZshrcOriginal::AddSourceFile'
